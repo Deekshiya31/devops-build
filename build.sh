a73fdf42997263bba01f67a3b380de4ee06e02ec
root <root@ip-172-31-43-101.ap-south-1.compute.internal>
2025-10-07 09:52:16 +0000

    Update scripts for DockerHub push

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker build -t react-app:dev .
+
+# Variables
+DOCKER_USER="deekshiya31"
+IMAGE_NAME="react-app"
+
+# Build the image
+docker build -t $DOCKER_USER/dev:latest .
+
+# Push to DockerHub dev repo
+docker push $DOCKER_USER/dev:latest
 
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-docker-compose down
-docker-compose up -d
+
+# Variables
+DOCKER_USER="deekshiya31"
+
+# Stop old container if running
+docker rm -f react-app || true
+
+# Pull latest image from DockerHub dev repo
+docker pull $DOCKER_USER/dev:latest
+
+# Run new container
+docker run -d -p 80:80 --name react-app $DOCKER_USER/dev:latest
 
